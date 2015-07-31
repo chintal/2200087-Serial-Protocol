9d550dc151eab9ac42287ea95294b9e38f7ca81e
Chintalagiri Shashank <shashank@chintal.in>
2015-07-31 20:35:57 +0530

    Tagged v0.5

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def read(fname):
 
 setup(
     name="driver2200087",
-    version="0.4",
+    version="0.5",
     author="Chintalagiri Shashank",
     author_email="shashank@chintal.in",
     description="Python Package to interface with Radio Shack's 2200087 Multimeter",
